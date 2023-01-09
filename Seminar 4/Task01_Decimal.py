e5fa12cf44a22a302c33acbf714c975ec45f8536
AvraamOvolono <avraamovolono@yandex.ru>
2023-01-10 02:19:27 +0300

    Add Task 01 Decimal project

---
+++ Seminar 4/Task01_Decimal.py
@@ -0,0 +1,15 @@
+# Вычислить число c заданной точностью d
+# 10^{-1} ≤ d ≤10^{-10}
+
+from math import pi
+
+max_accuracy = -1
+min_accuracy = -10
+
+def power_num(num):
+    for i in range(max_accuracy, min_accuracy - 1, -1):
+        if 10**i == num:
+            return -i
+
+d = float(input('Задайте точность: '))
+print('Число Пи с заданной точность', d, '=', round(pi, power_num(d)))
