c0d1f64b2dec3bfb203597ca9def41654a4bc135
AvraamOvolono <avraamovolono@yandex.ru>
2023-01-10 03:04:55 +0300

    Add Task 03 Set project

---
+++ Seminar 4/Task03_Set.py
@@ -0,0 +1,14 @@
+# Задайте последовательность чисел. Напишите программу, которая выведет
+# список неповторяющихся элементов исходной последовательности.
+
+from random import randint
+
+list_num = [randint(0,5) for i in range(randint(5,10))]
+list_unique = []
+
+for i in list_num:
+    if i not in list_unique:
+        list_unique.append(i)
+
+print(f'В нашем списке: {list_num} -\
+    \nимеются данные неповторяющиеся элементы: {list_unique}')
