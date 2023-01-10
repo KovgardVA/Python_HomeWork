027a3d0fe72790be06ccd8b96ba883231a2c0154
AvraamOvolono <avraamovolono@yandex.ru>
2023-01-10 04:46:43 +0300

    Seperate function from Task 04 to polynomial

--- Seminar 4/Task04_Polynomial.py
+++ Seminar 4/Task04_Polynomial.py
@@ -3,31 +3,7 @@
 # многочлен степени k.
 
 from random import randint
+from polynomial import fill
 
-def fill_poly(coef, file):
-    
-    k_list = [randint(0,100) for i in range(coef + 1)]
-    last_k = len(k_list)
-
-    data = open(file, 'w')
-
-    for i in range(last_k, 0, -1):
-        number = k_list[last_k - i]
-        if i == 2:
-            if number != 0:
-                data.writelines('X ' if number == 1 else f'{number}*X ')
-                if k_list[last_k - i + 1] != 0:
-                    data.writelines('+ ')
-        elif i == 1:
-            if number != 0:
-                    data.writelines(f'{number} ')
-            data.writelines('= 0')
-        else:
-            if number != 0:
-                data.writelines('X^{i - 1} ' if number == 1 else f'{number}*X^{i - 1} ')
-                if k_list[last_k - i + 1] != 0:
-                    data.writelines('+ ')
-
-    data.close()
-
-fill_poly(int(input('Введите натуральную степень многочлена: ')), 'poly.txt')
+k = int(input('Введите натуральную степень многочлена: '))
+fill(k, 'poly.txt')
