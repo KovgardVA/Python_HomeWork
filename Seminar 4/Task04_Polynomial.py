72f3bc90238ea365be3fd8bdca953ff974ca2705
AvraamOvolono <avraamovolono@yandex.ru>
2023-01-10 04:37:28 +0300

    Add function to Task 04 Polynomial project

--- Seminar 4/Task04_Polynomial.py
+++ Seminar 4/Task04_Polynomial.py
@@ -4,28 +4,30 @@
 
 from random import randint
 
-k = int(input('Введите натуральную степень многочлена: '))
-k_list = [randint(0,100) for i in range(k + 1)]
-last_k = len(k_list)
+def fill_poly(file, coef):
 
-path = 'poly.txt'
-data = open(path, 'w')
+    k_list = [randint(0,100) for i in range(coef + 1)]
+    last_k = len(k_list)
 
-for i in range(last_k, 0, -1):
-    number = k_list[last_k - i]
-    if i == 2:
-        if number != 0:
-            data.writelines('X ' if number == 1 else f'{number}*X ')
-            if k_list[last_k - i + 1] != 0:
-                data.writelines('+ ')
-    elif i == 1:
-        if number != 0:
-                data.writelines(f'{number} ')
-        data.writelines('= 0')
-    else:
-        if number != 0:
-            data.writelines('X^{i - 1} ' if number == 1 else f'{number}*X^{i - 1} ')
-            if k_list[last_k - i + 1] != 0:
-                data.writelines('+ ')
+    data = open(file, 'w')
 
-data.close()
+    for i in range(last_k, 0, -1):
+        number = k_list[last_k - i]
+        if i == 2:
+            if number != 0:
+                data.writelines('X ' if number == 1 else f'{number}*X ')
+                if k_list[last_k - i + 1] != 0:
+                    data.writelines('+ ')
+        elif i == 1:
+            if number != 0:
+                    data.writelines(f'{number} ')
+            data.writelines('= 0')
+        else:
+            if number != 0:
+                data.writelines('X^{i - 1} ' if number == 1 else f'{number}*X^{i - 1} ')
+                if k_list[last_k - i + 1] != 0:
+                    data.writelines('+ ')
+
+    data.close()
+
+fill_poly('poly.txt', int(input('Введите натуральную степень многочлена: ')))
