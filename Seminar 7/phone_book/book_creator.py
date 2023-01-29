3ca45083b88f52e306666f815474377d1cafece9
AvraamOvolono <avraamovolono@yandex.ru>
2023-01-29 16:48:26 +0300

    Fix the book creator modul

--- Seminar 7/phone_book/book_creator.py
+++ Seminar 7/phone_book/book_creator.py
@@ -1,33 +1,11 @@
 import data_provider as prov
 
-def add_id():
-    data = prov.get_id()
-    with open('phone_book.csv', 'a') as file:
-        file.writelines('{};'.format(data))
-
-def add_name():
-    data = prov.get_name()
-    with open('phone_book.csv', 'a') as file:
-        file.writelines('{};'.format(data))
-
-def add_surname():
-    data = prov.get_surname()
-    with open('phone_book.csv', 'a') as file:
-        file.writelines('{};'.format(data))
-
-def add_phone():
-    data = prov.get_phone()
-    with open('phone_book.csv', 'a') as file:
-        file.writelines('{};'.format(data))
-
-def add_comment():
-    data = prov.get_comment()
-    with open('phone_book.csv', 'a') as file:
-        file.writelines('{}\n'.format(data))
-
 def add_info():
-    add_id()
-    add_name()
-    add_surname()
-    add_phone()
-    add_comment()
+    data = ''
+    data += str(prov.get_id()) + ';'
+    data += str(prov.get_name()) + ';'
+    data += str(prov.get_surname()) + ';'
+    data += str(prov.get_phone()) + ';'
+    data += str(prov.get_comment()) + '\n'
+    with open('phone_book.csv', 'a') as file:
+        file.write(data)
