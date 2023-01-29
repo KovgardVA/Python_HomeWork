371048dc948211be17d26ea891736f92890f6c10
AvraamOvolono <avraamovolono@yandex.ru>
2023-01-29 17:42:04 +0300

    Optimized UI modul

--- Seminar 7/phone_book/book_creator.py
+++ Seminar 7/phone_book/book_creator.py
@@ -1,11 +1,11 @@
-import data_provider as prov
+import user_interface as ui
 
 def add_info():
     data = ''
-    data += str(prov.get_id()) + ';'
-    data += str(prov.get_name()) + ';'
-    data += str(prov.get_surname()) + ';'
-    data += str(prov.get_phone()) + ';'
-    data += str(prov.get_comment()) + '\n'
+    data += str(ui.get_num('Введите индентификационный номер: ')) + ';'
+    data += str(ui.get_string('Введите имя: ')) + ';'
+    data += str(ui.get_string('Введите фамилию: ')) + ';'
+    data += str(ui.get_num('Введите номер телефона: ')) + ';'
+    data += str(ui.get_string('Введите комментарий: ')) + '\n'
     with open('phone_book.csv', 'a') as file:
         file.write(data)
--- Seminar 7/phone_book/user_interface.py
+++ Seminar 7/phone_book/user_interface.py
@@ -11,3 +11,15 @@ def mode_question():
         mode = input('Желаете заполнить или прочитать файл?\n(w/r): ').lower()
     if mode == 'w': return True
     else: return False
+
+def get_string(comment):
+    temp = ''
+    while not temp.isalpha():
+        temp = input(comment)
+    return temp.title() 
+
+def get_num(comment):
+    num = ''
+    while not num.isdigit():
+        num = input(comment)
+    return num
