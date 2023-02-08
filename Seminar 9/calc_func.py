93f40fd2e1644b702ed5f8e307ba3bfaaff0a3ef
AvraamOvolono <avraamovolono@yandex.ru>
2023-02-08 22:03:12 +0300

    Upgraded Calc Bot

---
+++ Seminar 9/calc_func.py
@@ -0,0 +1,17 @@
+def get_sum(a, b):
+    return a + b
+
+def get_diff(a, b):
+    return a - b
+
+def get_prod(a, b):
+    return a * b
+
+def get_quot(a, b):
+    return a / b
+
+def get_int_quot(a, b):
+    return a // b
+
+def get_rem(a, b):
+    return a % b
--- Seminar 9/calc_main.py
+++ Seminar 9/calc_main.py
@@ -1,125 +1,78 @@
 import telebot
 from telebot import types
 from calc_log import log
+from calc_func import *
 
 TOKEN = '6048112543:AAEpsUduWoYEXiVTjTpW_WZBNdk83TaR7Ts'
 bot = telebot.TeleBot(TOKEN)
 
-@bot.message_handler(commands=['start'])
-def start_handler(message):
-    chat_id = message.chat.id
-    bot.send_message(chat_id, 'Добро пожаловать! Операции с какими цислами вы хотите произвести?\n/rational\n/complex')
+mode = None
+COMPLEX = 'complex'
+RATIO = 'ratio'
+NUM_1 = 0
+SIGN = 1
+NUM_2 = 2
 
-@bot.message_handler(commands=['rational', 'complex'])
-def button(message):
+@bot.message_handler(commands=['start'])
+def start_keyb(message):
     chat_id = message.chat.id
-    if message.text == '/rational':
-        ratio_markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        sum = types.KeyboardButton('+')
-        diff = types.KeyboardButton('-')
-        mult = types.KeyboardButton('*')
-        div = types.KeyboardButton('/')
-        rem = types.KeyboardButton('%')
-        div_int = types.KeyboardButton('//')
-        ratio_markup.add(sum, diff, mult, div, rem, div_int)
-        bot.send_message(chat_id, 'Выберите действие', reply_markup=ratio_markup)
-    else:
-        bot.send_message(chat_id, 'Stay tuned!')
-        start_handler(message)
+    mrk_modes = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    mode_1 = types.KeyboardButton('Рациональные')
+    mode_2 = types.KeyboardButton('Комплексные')
+    mrk_modes.add(mode_1, mode_2)
+    bot.send_message(chat_id, 'Операции с какими цислами вы хотите произвести?', reply_markup=mrk_modes)
 
 @bot.message_handler(content_types='text')
+def button(message):
+    global mode
+
+    chat_id = message.chat.id
+    if message.text == 'Рациональные':
+        mode = RATIO
+        bot.send_message(chat_id, 'Введите выражение через пробелы')
+        bot.register_next_step_handler(message, controller)
+    elif message.text == 'Комплексные':
+        mode = COMPLEX
+        bot.send_message(chat_id, 'Введите выражение через пробелы')
+        bot.register_next_step_handler(message, controller)
+    log(message)
+
 def controller(message):
     chat_id = message.chat.id
-    if message.text == '+':
-        msg = bot.send_message(chat_id, 'Введите числа через пробел')
-        bot.register_next_step_handler(msg, sum_num)
-    if message.text == '-':
-        msg = bot.send_message(chat_id, 'Введите числа через пробел')
-        bot.register_next_step_handler(msg, diff_num)
-    if message.text == '*':
-        msg = bot.send_message(chat_id, 'Введите числа через пробел')
-        bot.register_next_step_handler(msg, mult_num)
-    if message.text == '/':
-        msg = bot.send_message(chat_id, 'Введите числа через пробел')
-        bot.register_next_step_handler(msg, div_num)
-    if message.text == '%':
-        msg = bot.send_message(chat_id, 'Введите числа через пробел')
-        bot.register_next_step_handler(msg, rem_num)
-    if message.text == '//':
-        msg = bot.send_message(chat_id, 'Введите числа через пробел')
-        bot.register_next_step_handler(msg, int_div_num)
-    log(message)
 
-def sum_num(message):
-    items = message.text.split()
-    if len(items) != 2:
-        msg = bot.send_message(message.chat.id, 'Введите два числа')
-        bot.register_next_step_handler(msg, int_div_num)
+    math_exp = message.text.split()
+    if len(math_exp) != 3:
+        bot.send_message(chat_id, 'Выражение неверно. Попробуйте еще раз')
+        bot.register_next_step_handler(message, controller)
         return
-    else:
-        number_1 = int(items[0])
-        number_2 = int(items[1])
-        bot.send_message(message.chat.id, f'Сумма: {number_1 + number_2}')
-    log(message)
+    
+    math_sign = math_exp[SIGN]
+    if mode == RATIO:
+        a = int(math_exp[NUM_1])
+        b = int(math_exp[NUM_2])
+    elif mode == COMPLEX:
+        a = complex(math_exp[NUM_1])
+        b = complex(math_exp[NUM_2])
+    
+    if math_sign == '+':
+        result = get_sum(a, b)
+    elif math_sign == '-':
+        result = get_diff(a, b)
+    elif math_sign == '*':
+        result = get_prod(a, b)
+    elif math_sign == '/':
+        result = get_quot(a, b)
+    elif mode == COMPLEX and (math_sign == '//' or math_sign == '%'):
+        bot.send_message(chat_id, 'Невозможное выражение для комплексных чисел. Повторите ввод')
+        bot.register_next_step_handler(message, controller)
+        return
+    elif math_sign == '//':
+        result = get_int_quot(a, b)
+    elif math_sign == '%':
+        result = get_rem(a, b)
 
-def mult_num(message):
-    items = message.text.split()
-    if len(items) != 2:
-        msg = bot.send_message(message.chat.id, 'Введите два числа')
-        bot.register_next_step_handler(msg, int_div_num)
-        return
-    else:    
-        number_1 = int(items[0])
-        number_2 = int(items[1])
-        bot.send_message(message.chat.id, f'Произведение: {number_1 * number_2}')
-    log(message)
-
-def div_num(message):
-    items = message.text.split()
-    if len(items) != 2:
-        msg = bot.send_message(message.chat.id, 'Введите два числа')
-        bot.register_next_step_handler(msg, int_div_num)
-        return
-    else:    
-        number_1 = int(items[0])
-        number_2 = int(items[1])
-        bot.send_message(message.chat.id, f'Частное: {number_1 / number_2}')
-    log(message)
-
-def diff_num(message):
-    items = message.text.split()
-    if len(items) != 2:
-        msg = bot.send_message(message.chat.id, 'Введите два числа')
-        bot.register_next_step_handler(msg, int_div_num)
-        return
-    else:    
-        number_1 = int(items[0])
-        number_2 = int(items[1])
-        bot.send_message(message.chat.id, f'Разность: {number_1 - number_2}')
-    log(message)
-
-def rem_num(message):
-    items = message.text.split()
-    if len(items) != 2:
-        msg = bot.send_message(message.chat.id, 'Введите два числа')
-        bot.register_next_step_handler(msg, int_div_num)
-        return
-    else:    
-        number_1 = int(items[0])
-        number_2 = int(items[1])
-        bot.send_message(message.chat.id, f'Остаток от деления: {number_1 % number_2}')
-    log(message)
-
-def int_div_num(message):
-    items = message.text.split()
-    if len(items) != 2:
-        msg = bot.send_message(message.chat.id, 'Введите два числа')
-        bot.register_next_step_handler(msg, int_div_num)
-        return
-    else:    
-        number_1 = int(items[0])
-        number_2 = int(items[1])
-        bot.send_message(message.chat.id, f'Целочисленное частное: {number_1 // number_2}')
+    bot.send_message(chat_id, f'Результат вычислений: {result}')
+    start_keyb(message)
     log(message)
 
 bot.polling(none_stop=True)
